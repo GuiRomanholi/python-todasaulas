601c0fa755febdcb553466e63646fd3cd162789e
GuiRomanholi <guilhermeroma343@gmail.com>
2024-09-12 11:56:30 -0300

    agenda complexa e bem separadinha em firebase

---
+++ agenda_crud_firebase/controller.py
@@ -0,0 +1,24 @@
+from servico import Servico
+
+class Controller:
+    def __init__(self):
+        self.servico = Servico()
+
+    def cadastrar(self, contato):
+        print("Cadastrando o contato")
+        response = self.servico.cadastrar_firebase( contato )
+        print("Response: ", response)
+
+    def pesquisar(self):
+        print("Pesquisando contatos")
+        contatos = self.servico.pesquisar_firebase()
+        return contatos
+
+    def remover(self):
+        print("Remover um contato especifico")
+
+    def atualizar(self):
+        print("Atualizar um contato especifico")
+
+    def sair(self):
+        pass
